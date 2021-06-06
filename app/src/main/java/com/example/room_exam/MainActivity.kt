710e10cccf0c6afc93dd2aef1278ce9692858644
EomJunyoung <theoneday737@naver.com>
2021-06-06 16:08:25 +0900

    add func(databinding, viewbinding and modify error)

--- app/src/main/java/com/example/room_exam/MainActivity.kt
+++ app/src/main/java/com/example/room_exam/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.room_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.*
@@ -10,8 +11,9 @@ import com.example.room_exam.databinding.ActivityMainBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class   MainActivity : AppCompatActivity() {
 
+    //내가한 데이터바인딩
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
@@ -21,33 +23,40 @@ class MainActivity : AppCompatActivity() {
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
 
+        //강의에서한 데이터바인딩
+        val bindings = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        bindings.lifecycleOwner = this //livedata를 활용하기 위해서.. (refresh 위함)
+
 //        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         var viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        bindings.viewModel = viewModel
 
-        val db = Room.databaseBuilder(
-                applicationContext,
-                AppDatabase::class.java, "database-name"
-        ).build()
+
+//
+//        val db = Room.databaseBuilder(
+//                applicationContext,
+//                AppDatabase::class.java, "database-name"
+//        ).build()
 
         //TODO: 아래는 뷰모델이 아닌 직접접근할때
 //        db.todoDao().getAll().observe(this, Observer { todos ->
         //TODO: 아래는 뷰모델을 통해 접근
-        viewModel.getAll().observe(this, Observer { todos ->
+//        viewModel.getAll().observe(this, Observer { todos ->
+//
+//        binding.resultText.text = todos.toString()
+//
+//        })
 
-        binding.resultText.text = todos.toString()
 
-        })
-
-
-        binding.addButton.setOnClickListener {
-            //TODO: 아래는 db가 직접 insert
-//            db.todoDao().insert(Todo(binding.todoEdit.text.toString()))
-            //TODO: 아래는 뷰모델을 통해 insert를 지시
-
-            GlobalScope.launch {
-                viewModel.insert(Todo(binding.todoEdit.text.toString())) //insert가 suspend함수이기 떄문에 코루틴 밖에서 쓰면 안됨
-            }
-        }
+//        binding.addButton.setOnClickListener {
+//            //TODO: 아래는 db가 직접 insert
+////            db.todoDao().insert(Todo(binding.todoEdit.text.toString()))
+//            //TODO: 아래는 뷰모델을 통해 insert를 지시
+//
+//            GlobalScope.launch {
+//                viewModel.insert((binding.todoEdit.text.toString())) //insert가 suspend함수이기 떄문에 코루틴 밖에서 쓰면 안됨
+//            }
+//        }
 
     }
 }
